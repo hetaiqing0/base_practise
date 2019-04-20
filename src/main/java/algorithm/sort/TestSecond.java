565b45aa9a3423ab006f7674c1b36f220d9818e0
hetaiqing <33925681+hetaiqing0@users.noreply.github.com>
2019-04-20 16:42:04 +0800

    Update TestSecond.java

--- src/main/java/algorithm/sort/TestSecond.java
+++ src/main/java/algorithm/sort/TestSecond.java
@@ -2,6 +2,6 @@ package algorithm.sort;
 
 public class TestSecond {
     public static void main(String[] args) {
-        System.out.println("this second");
+        System.out.println("this second and modified");
     }
 }
