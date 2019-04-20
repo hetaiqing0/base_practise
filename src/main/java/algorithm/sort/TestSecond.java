0eb68e91de3d0609f455e4ec1bff9880d5f27544
hetaiqing <hanguangkuo@163.com>
2019-04-20 16:52:53 +0800

    add second

--- src/main/java/algorithm/sort/TestSecond.java
+++ src/main/java/algorithm/sort/TestSecond.java
@@ -2,6 +2,6 @@ package algorithm.sort;
 
 public class TestSecond {
     public static void main(String[] args) {
-        System.out.println("this second and modified and I also modified heng");
+        System.out.println("this second and modified and I also modified heng ha ha");
     }
 }
